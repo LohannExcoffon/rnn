6b0ec98b8e404aea49853e916e023a5578f4e87b
LohannExcoffon <lohann.excoffon@gmail.com>
2025-07-22 11:43:49 +0200

    comments and quick tweaks. added loss function for later

--- RNN.py
+++ RNN.py
@@ -21,37 +21,44 @@ class RNN():
         return 1 - np.tanh(x) ** 2
 
     def forward(self, inputs):
-        h_prev = np.zeros((hidden_size, 1))
-        xs, hs, ys = {}, {}, {}
-        hs[-1] = h_prev
+        h_prev = np.zeros((self.hidden_size, 1))
+        xs, hs, ys = {}, {}, {} #preallocate dicts to store states
+        hs[-1] = h_prev # no previous hidden layer
         for t in range(len(inputs)):
-            x_t = np.array([[inputs[t]]])
+            x_t = np.array([[inputs[t]]]) #current time step
             xs[t] = x_t
+
+            #matrix mult to calculate next hidden state based on input and previous hidden state
             h_t = self.tanh(np.dot(self.W_xh, x_t) + np.dot(self.W_hh, hs[t-1]) + self.b_h)
             hs[t] = h_t
-            y_t = np.dot(self.W_hy, h_t) + self.b_y
+
+            y_t = np.dot(self.W_hy, h_t) + self.b_y #get output state with updated hidden state
             ys[t] = y_t
         return xs, hs, ys
         
     def backward(self, xs, hs, ys, dy):
-        dW_xh = np.zeros_like(self.W_xh)
+        dW_xh = np.zeros_like(self.W_xh) #preallocate to store gradients
         dW_hh = np.zeros_like(self.W_hh)
         dW_hy = np.zeros_like(self.W_hy)
         db_h = np.zeros_like(self.b_h)
         db_y = np.zeros_like(self.b_y)
-        dh_next = np.zeros_like(hs[0])
 
-        for t in reversed(range(len(xs))):
+        dh_next = np.zeros_like(hs[0]) #no following hidden state
+        for t in reversed(range(len(xs))): #back propagation loop
             if t not in dy:
                 continue
             dW_hy += np.dot(dy[t], hs[t].T)
             db_y += dy[t]
+
             dh = np.dot(self.W_hy.T, dy[t]) + dh_next
             dt = dh * self.dtanh(hs[t])
+
             db_h += dt
             dW_xh += np.dot(dt, xs[t].T)
             dW_hh += np.dot(dt, hs[t-1].T)
+            
             dh_next = np.dot(self.W_hh.T, dt)
+        #clip gradients
         for dparam in [dW_xh, dW_hh, dW_hy, db_h, db_y]:
             np.clip(dparam, -1, 1, out=dparam)
         return dW_xh, dW_hh, dW_hy, db_h, db_y
@@ -62,3 +69,13 @@ class RNN():
         self.W_hy -= self.learning_rate * dW_hy
         self.b_h -= self.learning_rate * db_h
         self.b_y -= self.learning_rate * db_y
+
+
+def loss_and_derivatives(ys, target):
+    loss = 0
+    dy = {}
+    t = len(ys) - 1
+    y = ys[t]
+    loss += 0.5 * (y - target) ** 2
+    dy[t] = (y - target)
+    return loss, dy
