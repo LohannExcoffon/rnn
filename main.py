9edf9affbb043b61e153a0a66715fff2189cf2c2
Lohann Excoffon <lohann.excoffon@gmail.com>
2025-07-25 20:21:48 +0200

    testing hyperparameters for basic lstm

--- main.py
+++ main.py
@@ -37,30 +37,44 @@ def predict(start, length=5):
 
 
 # --------------- TRAIN ---------------
-rnn = RNN()
-for epoch in range(100):
-    total_loss = 0
-    for i in range(len(X)):
-        inputs = X[i]
-        target = np.array([[y[i]]]) #reshape as needed
+def train(model, epochs=100):
+    for epoch in range(epochs):
+        total_loss = 0
+        for i in range(len(X)):
+            inputs = X[i]
+            target = np.array([[y[i]]]) #reshape as needed
 
-        xs, hs, ys = rnn.forward(inputs)
-        loss, dy = loss_and_derivatives(ys, target)
-        dW_xh, dW_hh, dW_hy, db_h, db_y = rnn.backward(xs, hs, ys, dy)
-        rnn.update_params(dW_xh, dW_hh, dW_hy, db_h, db_y)
-        total_loss += loss.sum()
+            xs, hs, ys = model.forward(inputs)
+            loss, dy = loss_and_derivatives(ys, target)
+            dW_xh, dW_hh, dW_hy, db_h, db_y = model.backward(xs, hs, ys, dy)
+            model.update_params(dW_xh, dW_hh, dW_hy, db_h, db_y)
+            total_loss += loss.sum()
 
-    if epoch % 10 == 0:
-        print(f"Epoch {epoch}, Loss: {total_loss}")
-
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch}, Loss: {total_loss}")
 
 # --------------- TEST ---------------
-test_input = np.sin(np.linspace(2, 2.6, 10))
+def test(model, input):
+    # get predictions
+    xs, hs, ys = rnn.forward(input)
+    pred = ys[len(input) - 1]
+    print('Prediction is:', pred[0][0])
+    return pred[0][0]
+    
+
+
+test_input = np.sin(np.linspace(1, 3.6, 10))
 true_pred = test_input[-1]
 test_input = test_input[:-1]
 print(test_input)
 print(true_pred)
-# get predictions
-xs, hs, ys = rnn.forward(test_input)
-pred = ys[len(test_input) - 1]
-print('Prediction is:', float(pred))
+epochs = 300
+sizes = [50, 100, 200, 300, 400, 500, 1000]
+errors = {}
+for size in sizes:
+    rnn = RNN(input_size=1, hidden_size=size)
+    train(rnn, epochs)
+    output = test(rnn, test_input)
+    errors[size] = (true_pred - output)**2
+print(errors)
+    
